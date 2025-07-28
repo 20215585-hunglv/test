fd5a56ab675659c6158473ef22d4d52dab4de457
hungbkk66 <laiviethung2003>
2025-07-28 15:52:17 +0700

    fwfwfczx

--- test1.js
+++ test1.js
@@ -1,11 +1,4 @@
-const a = 3;
-const b = 4;
-const c = a + b;
-console.log(c);
-console.log("This is a test file.");
-console.log("End of test1.js");
-console.log("This is the end of the file.");
-console.log("Final output:", c);
-const d = 5;
+const a = 5;
+console.log(a);
 
 
